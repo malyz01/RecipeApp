d9ec17b549e5181a694fe987134dca5bf1824393
malyz <mattuy001@gmail.com>
2020-06-30 20:56:00 +1200

    added basic schema of users

--- server/db/models/Users.js
+++ server/db/models/Users.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
+const { validateEmail } = require('../../helpers');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
     unique: true,
-    required: 'is required',
+    required: 'please enter email',
     validate: {
       validator: validateEmail,
       message: (props) => `${props.value} is not a valid email address!`
     },
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please fill a valid email address'
+      'Please enter a valid email address'
     ]
   },
   password: {
     type: String,
     min: [8, 'Password is too short'],
-    required: 'is required'
+    required: 'please enter a password'
   },
   resetPasswordToken: String,
-  resetPasswordExpires: Date
+  resetPasswordExpires: Date,
+  resetEmailToken: String,
+  resetEmailExpires: Date
 });
 
 module.exports = mongoose.Model('Users', userSchema);
