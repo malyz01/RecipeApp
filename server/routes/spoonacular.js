7a4b8040e9e5608fba5985d09acb11c8f829a223
malyz <mattuy001@gmail.com>
2020-06-30 21:25:22 +1200

    added axios config in client

--- server/routes/spoonacular.js
+++ server/routes/spoonacular.js
@@ -1,3 +1,16 @@
+const axios = require('axios');
 const router = require('express').Router();
+
+const api = axios.create({
+  baseURL: process.env.FOOD_SPOONACULAR_URL,
+  params: {
+    apiKey: process.env.FOOD_SPOONACULAR_API
+  }
+});
+
+router.post('/', (req, res, next) => {
+  console.log(req.query);
+  res.json(req.body);
+});
 
 module.exports = router;
