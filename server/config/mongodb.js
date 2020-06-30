dde6771fd7fdd32ba5fe4ec243ed3c08bc8a7a01
malyz <mattuy001@gmail.com>
2020-06-30 20:43:26 +1200

    fixed mongodb config

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect(process.env.MONGODB, {
+mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
+
+const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
