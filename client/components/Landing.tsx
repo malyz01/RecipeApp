5ac692719d167c73eb29e23895a3f485c679fddc
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-03 14:29:54 +1200

    update search in handleClick in landing component

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -24,7 +24,7 @@ const Landing = (props) => {
   };
 
   const handleClick = (query: i.ISearch) => () => {
-    props.fetchRecipesBy(e.Params.search, { params: query });
+    props.fetchRecipesBy('search', { params: query });
   };
 
   return (
