cdb2b35d43858d3a61335ace4eda070f2aca093e
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-08 15:00:58 +1200

    create search component with first input field

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Recipe from './Recipe';
+import Search from './Search'
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Landing page</h1>
-      <Recipe />
+      {/* <h1>Landing page</h1> */}
+      {/* <Recipe /> */}
+      <Search />
     </div>
   );
 };
---
+++ client/components/Search/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <div className='searchMainContainer'>
+      <div className='searchField'>
+        <h3>Search by Recipe Name</h3>
+        <input type='text' placeholder='e.g. Chicken Curry'></input>
+      </div>
+    </div>
+  )
+}
+
+export default index
