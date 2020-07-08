7eb21227be4287fe98d69083d75de9af4c5ff28d
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-08 15:10:15 +1200

    add 'add' buttons and clear all text in ingredients search

--- client/components/Search/index.tsx
+++ client/components/Search/index.tsx
@@ -4,16 +4,24 @@ import React from 'react'
 const index = () => {
   return (
     <div className='searchMainContainer'>
-      <div className='searchField'>
+      <div className='searchRecipe'>
         <h3>Search by Recipe Name</h3>
         <input type='text' placeholder='e.g. Chicken Curry'></input>
       </div>
-      <div className='ingredientFilter'>
+      <div className='searchIngredients'>
         <h3>Search by Ingredients</h3>
-        <h4>Included Ingredients:</h4>
-        <input type='text' placeholder='e.g. Potato'></input>
-        <h4>Excluded Ingredients:</h4>
-        <input type='text' placeholder='e.g. Coriander'></input>
+        <div className='ingredientFilter'>
+          <h4>Included Ingredients:</h4>
+          <input type='text' placeholder='e.g. Potato'></input>
+          <button>Add</button>
+          <a>clear all</a>
+        </div>
+        <div className='ingredientFilter'>
+          <h4>Excluded Ingredients:</h4>
+          <input type='text' placeholder='e.g. Coriander'></input>
+          <button>Add</button>
+          <a>clear all</a>
+        </div>
       </div>
     </div>
   )
