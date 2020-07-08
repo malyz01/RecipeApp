c527d7b56d8d8ebb07a46ab4c20cd98e042f19a7
malyz <mattuy001@gmail.com>
2020-07-08 18:29:01 +1200

    added sample slider

--- client/components/Search/index.tsx
+++ client/components/Search/index.tsx
@@ -1,58 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Slider from './Slider';
 
-import './search.css'
+import './search.css';
 
 const index = () => {
   return (
-    <div className='searchMainContainer'>
-
-      <div className='searchRecipe'>
-        <h3 className='searchHeading'>Search by Recipe Name</h3>
-        <div className='searchInput'>
-          <input className='foodSearch' type='text' placeholder='e.g. Chicken Curry'></input>
+    <div className="searchMainContainer">
+      <div className="searchRecipe">
+        <h3 className="searchHeading">Search by Recipe Name</h3>
+        <div className="searchInput">
+          <input className="foodSearch" type="text" placeholder="e.g. Chicken Curry"></input>
         </div>
       </div>
 
-      <div className='searchIngredients'>
+      <div className="searchIngredients">
         <h3>Search by Ingredients</h3>
-        <div className='ingredientFilter'>
-          <h4 className='searchHeading'>Included Ingredients:</h4>
-          <div className='ingredientSearch'>
+        <div className="ingredientFilter">
+          <h4 className="searchHeading">Included Ingredients:</h4>
+          <div className="ingredientSearch">
             <div>
-              <input type='text' placeholder='e.g. Potato'></input>
+              <input type="text" placeholder="e.g. Potato"></input>
               <button>Add</button>
             </div>
-            <div>
-              clear all
-              </div>
+            <div>clear all</div>
           </div>
         </div>
 
-
-        <div className='ingredientFilter'>
-          <h4 className='searchHeading'>Excluded Ingredients:</h4>
-          <div className='ingredientSearch'>
+        <div className="ingredientFilter">
+          <h4 className="searchHeading">Excluded Ingredients:</h4>
+          <div className="ingredientSearch">
             <div>
-              <input type='text' placeholder='e.g. Coriander'></input>
+              <input type="text" placeholder="e.g. Coriander"></input>
               <button>Add</button>
             </div>
-            <div>
-              clear all
-              </div>
+            <div>clear all</div>
           </div>
         </div>
       </div>
 
-      <div className='NutrionalInfo'>
+      <div className="NutrionalInfo">
         <h3>Nutrional Information</h3>
+        <Slider />
       </div>
 
       <div>
-        <button className='searchButton'>Search</button>
+        <button className="searchButton">Search</button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
