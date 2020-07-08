718b11ac540422739a6b2a82082f14d91f00e3fd
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-08 17:04:13 +1200

    fix input operlapping issue

--- client/components/Search/index.tsx
+++ client/components/Search/index.tsx
@@ -6,37 +6,51 @@ const index = () => {
   return (
     <div className='searchMainContainer'>
 
-      <div className='searchRecipe'>
-        <h3 className='searchHeading'>Search by Recipe Name</h3>
-        <div className='searchInput'>
-          <input className='foodSearch' type='text' placeholder='e.g. Chicken Curry'></input>
-        </div>
+      {/* <div className='searchRecipe'> */}
+      <h3 className='searchHeading'>Search by Recipe Name</h3>
+      <div className='searchInput'>
+        <input className='foodSearch' type='text' placeholder='e.g. Chicken Curry'></input>
       </div>
+      {/* </div> */}
 
       <div className='searchIngredients'>
         <h3>Search by Ingredients</h3>
         <div className='ingredientFilter'>
-          <h4>Included Ingredients:</h4>
-          <input type='text' placeholder='e.g. Potato'></input>
-          <button>Add</button>
-          <a>clear all</a>
+          <h4 className='searchHeading'>Included Ingredients:</h4>
+          <div className='ingredientSearch'>
+            <div>
+              <input type='text' placeholder='e.g. Potato'></input>
+              <button>Add</button>
+            </div>
+            <div>
+              clear all
+              </div>
+          </div>
         </div>
+
+
         <div className='ingredientFilter'>
-          <h4>Excluded Ingredients:</h4>
-          <input type='text' placeholder='e.g. Coriander'></input>
-          <button>Add</button>
-          <a>clear all</a>
+          <h4 className='searchHeading'>Excluded Ingredients:</h4>
+          <div className='ingredientSearch'>
+            <div>
+              <input type='text' placeholder='e.g. Coriander'></input>
+              <button>Add</button>
+            </div>
+            <div>
+              clear all
+              </div>
+          </div>
         </div>
-
-        <div className='NutrionalInfo'>
-          <h3>Nutrional Information</h3>
-        </div>
-
-        <div className='searchButton'>
-          <button>Search</button>
-        </div>
-
       </div>
+
+      <div className='NutrionalInfo'>
+        <h3>Nutrional Information</h3>
+      </div>
+
+      <div>
+        <button className='searchButton'>Search</button>
+      </div>
+
     </div>
   )
 }
