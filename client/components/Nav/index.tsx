9facc2b7dc681ac26d788e940473145414692fbe
malyz <mattuy001@gmail.com>
2020-07-12 18:26:47 +1200

    added some styling for nav

--- client/components/Nav/index.tsx
+++ client/components/Nav/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Typo from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { sIndex } from './styles';
 
 const index = () => {
   const c = sIndex();
+  const handleClick = () => {
+    alert('add functionality');
+  };
+
   return (
     <div className={c.mainContainer}>
       <Typo variant="h6">Recipe app</Typo>
-      <MoreVertIcon fontSize="small" />
+      <IconButton className={c.iconButton}>
+        <MoreVertIcon onClick={handleClick} />
+      </IconButton>
     </div>
   );
 };
--- client/components/Nav/styles.ts
+++ client/components/Nav/styles.ts
@@ -4,12 +4,15 @@ export const sIndex = makeStyles((theme: Theme) =>
   createStyles({
     mainContainer: {
       height: 80,
-      backgroundColor: theme.palette.primary.main,
+      backgroundColor: theme.palette.secondary.main,
       color: 'white',
       display: 'flex',
       justifyContent: 'space-between',
       alignItems: 'center',
       padding: '0 3em'
+    },
+    iconButton: {
+      color: 'white'
     }
   })
 );
