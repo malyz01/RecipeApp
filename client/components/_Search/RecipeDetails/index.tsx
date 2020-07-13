0786e9be6151ccabba5fc2d9206fce1530063656
malyz <mattuy001@gmail.com>
2020-07-13 15:58:21 +1200

    created simple layout for recipe details

--- client/components/_Search/RecipeDetails/index.tsx
+++ client/components/_Search/RecipeDetails/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { sIndex } from './styles';
 
 const index = () => {
-  return <div>Main container for Recipe details</div>;
+  return (
+    <div>
+      <img src={'#'} />
+      <div style={{ display: 'flex' }}>
+        <div>
+          <div>Chicken Tikka</div>
+          <div>Time: 30mins</div>
+          <div>Servings: 2</div>
+        </div>
+        <div>You have all the ingredients</div>
+      </div>
+    </div>
+  );
 };
 
 export default index;
