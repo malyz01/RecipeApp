688133de304f4c143545b17be0a190ca0abb31e6
malyz <mattuy001@gmail.com>
2020-07-18 14:34:40 +1200

    added recipe details

---
+++ client/components/_RecipeDetail/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <div>
+        <h4>Recipe Details</h4>
+        <p>Source: </p>
+        <p>Image </p>
+        <p>Etc </p>
+      </div>
+      <div>Ingredients component</div>
+      <div>Instructions component</div>
+    </div>
+  );
+};
+
+export default index;
