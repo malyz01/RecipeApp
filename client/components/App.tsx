dd14b1887ee90275f4c3b9d6ef6e52340dae15c4
malyz <mattuy001@gmail.com>
2020-07-24 15:54:39 +1200

    NEW: added component url does not exists

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import Landing from './Landing';
 import Search from './_Search';
+import UDE from './_UrlDoesNotExist';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/recipes/search" component={Search} />
+        <Route exact path="*" component={UDE} />
       </Switch>
     </BrowserRouter>
   );
