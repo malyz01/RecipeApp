3fa262e0a18b266dd6d3dfe167c9deccb7108ee2
malyz <mattuy001@gmail.com>
2020-07-26 13:06:44 +1200

    UPDATE: added new sample test for server and client side

--- tests/client/app.test.js
+++ tests/client/app.test.js
@@ -4,7 +4,8 @@ import { mount } from 'enzyme';
 import App from '../../client/components/App';
 
 test('<App />', () => {
-  const expected = 'React development has begun!';
+  const expected = 'Recipe appLanding page';
   const wrapper = mount(<App />);
+  console.log(Object.keys(wrapper));
   expect(wrapper.text()).toMatch(expected);
 });
---
+++ tests/server/db/users.test.js
@@ -0,0 +1,5 @@
+describe('Read', () => {
+  it('should read a doc from collection', async () => {
+    expect(true).toBeTruthy();
+  });
+});
