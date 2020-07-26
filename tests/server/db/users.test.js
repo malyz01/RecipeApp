43c2a5e194ebafedfd4898fcd4ba4a4561ff760f
malyz <mattuy001@gmail.com>
2020-07-26 14:49:32 +1200

    UPDATE: added jest-mongodb config and sample user test

--- tests/server/db/users.test.js
+++ tests/server/db/users.test.js
@@ -1,5 +1,26 @@
-describe('Read', () => {
+const mongoose = require('mongoose');
+const UserModel = require('../../../server/db/models/Users');
+
+const userData = { email: 'sample@gmail.com', password: '1234' };
+
+// TODO fix test
+describe('User Model Test', () => {
+  beforeAll(async () => {
+    await mongoose.connect(
+      global.__MONGO_URI__,
+      { useNewUrlParser: true, useCreateIndex: true },
+      (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      }
+    );
+  });
   it('should read a doc from collection', async () => {
-    expect(true).toBeTruthy();
+    const savedUser = await UserModel.create(userData);
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.email).toBe(userData.email);
   });
 });
+q;
