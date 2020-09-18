b1e67b912bc20f8ccad378450b568225b069f2f3
malyz <mattuy001@gmail.com>
2020-09-18 22:15:09 +1200

    Moved

---
+++ client/pages/Index.ts
@@ -0,0 +1,11 @@
+import Landing from './Landing';
+import RecipeDetail from './RecipeDetail';
+import Search from './Search';
+import UrlDNE from './_UrlDoesNotExist';
+
+export default {
+  Landing,
+  RecipeDetail,
+  Search,
+  UrlDNE
+};
