530b10fe31d9d2672982997bb5052818039e2739
malyz <mattuy001@gmail.com>
2020-09-18 22:12:58 +1200

    Changed home page

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Nav from './Nav';
 import Pages from './_Pages';
+import Home from '../pages/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Pages.Landing} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/recipes/search" component={Pages.Search} />
         <Route exact path="/recipes/:id" component={Pages.RecipeDetail} />
         <Route exact path="*" component={Pages.UrlDNE} />
