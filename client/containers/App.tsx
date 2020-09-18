88430828d287686181d2e6a470918fd60487443a
malyz <mattuy001@gmail.com>
2020-09-18 22:36:36 +1200

    Changed Nav import location

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Nav from './Nav';
+import Nav from '../components/layout/Nav';
 import Pages from './_Pages';
 import Home from '../pages/Home';
 
