4314707c536aea46a34611e65a459fe31dd3b6fb
malyz <mattuy001@gmail.com>
2020-09-18 22:15:22 +1200

    Created notfound component

---
+++ client/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 400 }}
+    >
+      <h1>Sorry! URL does not exist!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
