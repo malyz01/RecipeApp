42cb28420f461984f196b38e47e41419fcf4e3b4
malyz <mattuy001@gmail.com>
2020-09-19 17:37:17 +1200

    Updated content

--- client/containers/Search/FilterByIngredient.tsx
+++ client/containers/Search/FilterByIngredient.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -12,22 +12,21 @@ const FilterByIngredient: React.FC<Props> = (props) => {
   return (
     <Accordion>
       <AccordionSummary IconButtonProps={{ size: 'small' }} expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="subtitle2">{name} Ingredients:</Typography>
+        <Typography variant="subtitle2">{props.summary}</Typography>
       </AccordionSummary>
       <AccordionDetails>{props.children}</AccordionDetails>
     </Accordion>
   );
 };
 
-const mapState = (state) => ({
-  includeIngredients: state.queries.includeIngredients,
-  excludeIngredients: state.queries.excludeIngredients
-});
 const mapDispatch = { ...queries };
 
-const connector = connect(mapState, mapDispatch);
+const connector = connect(null, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-interface Props extends PropsFromRedux {}
+interface Props extends PropsFromRedux {
+  children: any;
+  summary: string;
+}
 
 export default connector(FilterByIngredient);
