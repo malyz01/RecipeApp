8ba0e609fe7caffa5634ed7adba5cef0063aeb2c
malyz <mattuy001@gmail.com>
2020-09-19 19:03:19 +1200

    Applied styles

--- client/containers/Search/styles/FilterInput.ts
+++ client/containers/Search/styles/FilterInput.ts
@@ -2,16 +2,29 @@ import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme: Theme) =>
   createStyles({
+    main: {
+      width: '100%'
+    },
     section: {
       display: 'flex',
       marginBottom: '1rem'
+    },
+    inputContainer: {
+      justifyContent: 'space-between'
+    },
+    input: {
+      maxHeight: 40,
+      borderRight: 'none'
+    },
+    addBtn: {
+      height: '100%'
     },
     paper: {
       display: 'flex',
       alignItems: 'center',
       padding: '.5rem 1rem',
       margin: '0 .5rem .5rem 0',
-      background: 'green'
+      background: theme.palette.primary.main
     },
     closeIcon: {
       cursor: 'pointer'
