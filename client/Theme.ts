f19f2bd8afb0bdb7def60633a991765b0ab3d84b
malyz <mattuy001@gmail.com>
2020-09-19 19:02:39 +1200

    Change primary color

--- client/Theme.ts
+++ client/Theme.ts
@@ -1,22 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { blueGrey } from '@material-ui/core/colors';
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'IBM Plex Sans'
+  },
   palette: {
     primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#ff4400'
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
+      main: '#C8E27C'
     },
     secondary: {
       main: blueGrey[900]
     },
-    // Used by `getContrastText()` to maximize the contrast between
-    // the background and the text.
     contrastThreshold: 3,
-    // Used by the functions below to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
     tonalOffset: 0.2
   }
 });
