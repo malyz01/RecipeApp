4e156aea59f0e54bb91c29b0413e782d370928b9
malyz <mattuy001@gmail.com>
2020-09-22 22:06:30 +1200

    Added styles

--- client/components/Accordion.tsx
+++ client/components/Accordion.tsx
@@ -4,12 +4,25 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      color: theme.palette.primary.main
+    }
+  })
+);
 
 const CustomAccordion: React.FC<Props> = (props) => {
+  const c = useStyles();
+
   return (
-    <Accordion elevation={3}>
+    <Accordion className={c.mainContainer} elevation={3}>
       <AccordionSummary IconButtonProps={{ size: 'small' }} expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="subtitle2">{props.summary}</Typography>
+        <Typography variant="subtitle2">
+          <strong>{props.summary}</strong>
+        </Typography>
       </AccordionSummary>
       <AccordionDetails>{props.children}</AccordionDetails>
     </Accordion>
