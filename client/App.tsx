d516b9f77a17b9c2a95137d41f9721b73c1822e7
malyz <mattuy001@gmail.com>
2020-09-22 21:52:43 +1200

    Moved App

---
+++ client/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Nav from './components/layout/Nav';
+import Page from './pages';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Page.Home} />
+        <Route exact path="*" component={Page.NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
