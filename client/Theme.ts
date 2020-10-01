fd2af0d1578717eb6a31fb552f24a787bf20e0bf
malyz <mattuy001@gmail.com>
2020-10-01 20:13:32 +1300

    Removed bluegray

--- client/Theme.ts
+++ client/Theme.ts
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { blueGrey } from '@material-ui/core/colors';
+
 const theme = createMuiTheme({
   typography: {
     fontFamily: 'IBM Plex Sans'
