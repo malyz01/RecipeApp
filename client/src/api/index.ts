c71b8b0d189473cfcff838187208915ac79d5a71
malyz <mattuy001@gmail.com>
2020-11-15 22:05:17 +1300

    intial commit

---
+++ client/src/api/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: '/api/v1'
+});
+
+export const spoonacular = axios.create({
+  baseURL: '/api/v1/spoonacular/recipes/'
+});
